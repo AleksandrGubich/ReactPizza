084e57e716ad53914173731a2a8305d47b7657af
Aliaksandr Hubich <a.gubich@bk.ru>
2025-10-09 15:36:40 +0200

    .

--- react-pizza/src/App.js
+++ react-pizza/src/App.js
@@ -1,12 +1,19 @@
+import React from "react";
 import Categories from "./components/Categories/Categories";
 import Header from "./components/Header/Header";
 import PizzaBlock from "./components/PizzaBlock/PizzaBlock";
 import Sort from "./components/Sort/Sort";
 import "./scss/app.scss";
 
-import pizzas from "./assets/pizzas.json";
-
 function App() {
+  const [items, setItems] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("https://68e7a27e10e3f82fbf400722.mockapi.io/items")
+      .then((res) => res.json())
+      .then((arr) => setItems(arr));
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
@@ -18,7 +25,7 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {pizzas.map((obj) => (
+            {items.map((obj) => (
               <PizzaBlock key={obj.id} {...obj} />
             ))}
           </div>

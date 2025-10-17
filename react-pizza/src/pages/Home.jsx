690978a9853882a6ef248c16154aada28e296e94
Aliaksandr Hubich <a.gubich@bk.ru>
2025-10-17 16:13:51 +0200

    .

--- react-pizza/src/pages/Home.jsx
+++ react-pizza/src/pages/Home.jsx
@@ -4,11 +4,13 @@ import Categories from "../components/Categories/Categories";
 import PizzaBlockSkeleton from "../components/PizzaBlockSkeleton/PizzaBlockSkeleton";
 import PizzaBlock from "../components/PizzaBlock/PizzaBlock";
 import Sort from "../components/Sort/Sort";
+import Pagination from "../components/Pagination/Pagination";
 
 const Home = ({ searchValue }) => {
   const [items, setItems] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
   const [categoryID, setCategoryID] = React.useState(0);
+  const [currentPage, setCurrentPage] = React.useState(1);
   const [sortType, setSortType] = React.useState({
     name: "популярности (DESC)",
     sortProperty: "rating",
@@ -23,7 +25,7 @@ const Home = ({ searchValue }) => {
     const search = searchValue ? `&search=${searchValue}` : "";
 
     fetch(
-      `https://68e7a27e10e3f82fbf400722.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}${search}`
+      `https://68e7a27e10e3f82fbf400722.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`
     )
       .then((res) => res.json())
       .then((arr) => {
@@ -31,7 +33,7 @@ const Home = ({ searchValue }) => {
         setIsLoading(false);
       });
     window.scrollTo(0, 0);
-  }, [categoryID, sortType, searchValue]);
+  }, [categoryID, sortType, searchValue, currentPage]);
 
   const pizzas = items.map((obj) => <PizzaBlock key={obj.id} {...obj} />);
   const skeletons = [...new Array(6)].map((_, index) => (
@@ -49,6 +51,7 @@ const Home = ({ searchValue }) => {
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">{isLoading ? skeletons : pizzas}</div>
+      <Pagination onChangePage={(number) => setCurrentPage(number)} />
     </div>
   );
 };
